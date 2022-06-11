d01aafdb74853819d6bd1582af78ee8e0d35c4b8
IronWil1 <aidarziyangirov@gmail.com>
2022-06-11 21:46:52 +0300

    Second commit Cat Scope

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat1 = applicationContext.getBean(Cat.class);
+        Cat beanCat2 = applicationContext.getBean(Cat.class);
+        System.out.println(beanCat1.getName());
+        System.out.println(beanCat2.getName());
+
+        System.out.println(bean==bean2);
+        System.out.println(beanCat1==beanCat2);
+
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name = "cat")
+    @Scope(scopeName = "prototype")
+    public Cat getCatName() {
+        Cat cat = new Cat();
+        cat.setName("Cat");
+        return cat;
+    }
 }
